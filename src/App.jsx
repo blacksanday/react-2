1601cb714a10c2cb5772080a39d3d365f5e0b578
black.0190@icloud.com <black.0190@icloud.com>
2023-10-30 20:27:41 +0900

    fa

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 
 function randomValueFromArray(array) {
@@ -12,63 +12,71 @@ export default function App() {
   const [xItem, setXItem] = useState("");
   const [yItem, setYItem] = useState("");
   const [zItem, setZItem] = useState("");
-  const [ukus, setUkus] = useState("us") ;
+  const [ukus, setUkus] = useState("us");
+  const nameInputRef = useRef();
+  const ukusRef = useRef();
+
 
   function generateRandomStory(event) {
     event.preventDefault();
     const xItemArray = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
     const yItemArray = ["the soup kictchen", "Disneyland", "the White House"];
     const zItemArray = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
-    
+
     setXItem(randomValueFromArray(xItemArray));
     setYItem(randomValueFromArray(yItemArray));
     setZItem(randomValueFromArray(zItemArray));
+
+    const inputName = nameInputRef.current.value;
+    setName(inputName || "Bob");
+
     setShowStory(true);
+    ukusRef.current=ukus;
+  
   }
 
-  function handleName(event) {
-    const inputName = event.target.value;
-    setName(inputName);
-  }
 
-  function checkedUkus(event){
+
+
+  function checkedUkus(event) {
     setUkus(event.target.value);
   }
 
-  function convertCelsius(temprature){
-      return temprature = (temprature - 32) * 0.555 ;
+  function convertCelsius(temprature) {
+    return (temprature - 32) * 0.555;
   }
 
-  function convertStone(weighs){
-    return weighs = weighs * 0.071429;
+  function convertStone(weighs) {
+    return weighs * 0.071429;
   }
-  
 
 
   return (
     <>
-      <div class="form">
-  
-        <label htmlFor="customname">Enter custom name:</label>
-        <input type="text" placeholder="" onChange={handleName} />
-      </div>
-      <div>
-        <label htmlFor="us" >US</label>
-        <input type="radio" value="us" checked={ukus === "us"} onClick={checkedUkus}/>
-        <label htmlFor="uk" >UK</label>
-        <input type="radio" value="uk" checked={ukus === "uk"} onClick={checkedUkus}/>
-      </div>
-      <div>
-        <button onClick={generateRandomStory}>Generate random story</button>
-      </div>
-      {showStory && (
-        <p>
-          It was {ukus == "us" ? 94 : Math.round(convertCelsius(94))}{ukus === "us" ?  " fahrenheit" : "celsius"} outside, so {xItem} went for a walk. When they
-          got to {yItem}, they stared in horror for a few moments, then {zItem}.
-          {name || "Bob"} saw the whole thing, but was not surprised — {xItem} weighs {ukus === "us" ? 300 :Math.round(convertStone(300))}
-          {ukus === "us" ? "pounds" : "stone"}, and it was a hot day.
-        </p>
-      )}
+      <form onSubmit={generateRandomStory}>
+        <div>
+          <label htmlFor="customname">Enter custom name:</label>
+          <input type="text" placeholder="" ref={nameInputRef} />
+        </div>
+        <div>
+          <label htmlFor="us" >US</label>
+          <input type="radio" value="us" checked={ukus === "us"} onClick={checkedUkus}  />
+          <label htmlFor="uk" >UK</label>
+          <input type="radio" value="uk" checked={ukus === "uk"} onClick={checkedUkus} />
+        </div>
+        <div>
+
+          <button type="submit">Generate random story</button>
+        </div>
+        {showStory && (
+          <p>
+            It was {ukusRef.current == "us" ? 94 : Math.round(convertCelsius(94))}{ukus === "us" ? " fahrenheit" : "centigrade"} outside, so {xItem} went for a walk. When they
+            got to {yItem}, they stared in horror for a few moments, then {zItem}.
+            {name} saw the whole thing, but was not surprised — {xItem} weighs {ukus === "us" ? 300 : Math.round(convertStone(300))}
+            {ukusRef.current === "us" ? "pounds" : "stone"}, and it was a hot day.
+          </p>
+        )}
+      </form>
     </>
   );
 }
